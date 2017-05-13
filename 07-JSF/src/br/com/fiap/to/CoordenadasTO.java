e3fc529e30525ce10be63f06592ec7232ba61e02
edipoalmeida <almeida.edipo@icloud.com>
2017-05-13 17:50:25 -0300

    finalizando projeto de JSF Restful

---
+++ 07-JSF/src/br/com/fiap/to/CoordenadasTO.java
@@ -0,0 +1,58 @@
+package br.com.fiap.to;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class CoordenadasTO {
+
+	private int codigo;
+
+	private double latitude;
+
+	private double longitude;
+
+	private String nomeLocal;
+
+	private String descricao;
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getNomeLocal() {
+		return nomeLocal;
+	}
+
+	public void setNomeLocal(String nomeLocal) {
+		this.nomeLocal = nomeLocal;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
